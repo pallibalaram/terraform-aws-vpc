c28db12d13326e0decb174e2cf2d834f508cc0db
pallibalaram <158497199+pallibalaram@users.noreply.github.com>
2025-05-11 15:58:08 +0530

    files are committed to terraform aws vpc

---
+++ variables.tf
@@ -0,0 +1,72 @@
+variable "project_name" {
+    type =string
+}
+variable "environment" {
+    type = string
+}
+
+variable "vpc_cidr" {
+    type = string
+    default = "10.0.0.0/16"
+}
+variable "enable_dns_hostnames"{
+    default = "true"
+    type = bool
+}
+variable "common_tags" {
+    type = map
+    default = {}
+}
+variable "vpc_tags" {
+    default = {}
+}
+variable "igw_tags" {
+    default = {}
+}
+
+variable "public_subnets_cidr" {
+    type = list
+    validation {
+       condition     = length(var.public_subnets_cidr) == 2 
+       error_message = "please give 2  public valid subnet CIDR "
+    }
+}
+variable "public_subnets_tags"{
+    default = {}
+}
+
+variable "private_subnets_cidr"{
+    type = list
+    validation {
+       condition     = length(var.private_subnets_cidr) == 2 
+       error_message = "please give 2  private valid subnet CIDR "
+    }
+}
+variable "private_subnets_tags"{
+    default = {}
+}
+
+variable "database_subnets_cidr"{
+    type = list
+    validation {
+        condition     = length(var.database_subnets_cidr) == 2
+        error_message = "please give 2 valid database subnets CIDR"
+    }
+}
+variable "database_subnets_tags" {
+    default = {}
+}
+
+variable "public_route_table_tags" {
+    default = {}
+}
+variable "private_route_table_tags" {
+    default = {}
+}
+variable "database_route_table_tags" {
+    default = {}
+}
+
+variable "nat_gateway_tags"{
+    default = {}
+}
